367ab591ca85bd29a6e521cef372ffca359d5479
alejandroFerranteGlobant <ale.ferrante@globant.com>
2016-10-21 12:25:12 -0300

    Updated file to make upload requests. main func was changed to ain2to avoid problems

--- fileUpload.go
+++ fileUpload.go
@@ -8,9 +8,12 @@ import (
   "mime/multipart"
   "net/http"
   "os"
-  "path/filepath"
+  //"path/filepath"
+  "strconv"
+  "io/ioutil"
 )
 
+/*
 // Creates a new file upload http request with optional extra params
 func newfileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
   file, err := os.Open(path)
@@ -39,15 +42,8 @@ func newfileUploadRequest(uri string, params map[string]string, paramName, path 
   req.Header.Set("Content-Type", writer.FormDataContentType())
   return req, err
 }
-
-func createFileUploadRequest(targetURL string, paramName, path string,fileRepeat int) (*http.Request, error) {
-  
-  //GET FILE HANDLER
-  file, err := os.Open(path)
-  if err != nil {
-      return nil, err
-  }
-  defer file.Close()
+*/
+func createFileUploadRequest(targetURL, path string,fileRepeat int) (*http.Request, error) {
 
   //CREATE BODY
   requestBody := &bytes.Buffer{}
@@ -55,35 +51,62 @@ func createFileUploadRequest(targetURL string, paramName, path string,fileRepeat
   //CREATE WRITER FOR WRITER
   writer := multipart.NewWriter(requestBody)
 
- i := 0
- for(i<fileRepeat){
-    //WRITE PART
-    part, err := writer.CreateFormFile(fmt.Sprintf("%s_%d",paramName,i), filepath.Base(path))
-    if err != nil {
-        return nil, err
-    }
-    _, err = io.Copy(part, file)
-    i++
- }
+ 
+    files, _ := ioutil.ReadDir(path)
+    for _, fileInfo := range files {
+        i := 0
+        for(i<fileRepeat){
 
+            //WRITE PART
+            name := fileInfo.Name()
+            fullPath := fmt.Sprint(path,"/",fileInfo.Name())
+            part, formCreationError := writer.CreateFormFile( name , fullPath )
+            if formCreationError != nil {
+                return nil, formCreationError
+            }
+
+            //GET FILE HANDLER
+            file, fileOpenError := os.Open(fullPath)
+            if fileOpenError != nil {
+                return nil, fileOpenError
+            }
+            defer file.Close()
+
+            //COPY CONTENT
+            _, fileCopyError := io.Copy(part, file)
+            if(fileCopyError != nil){
+                return nil, fileCopyError
+            }
+
+        i++
+        }
+
+
+    }
+  
   //CLOSE WRITER
-  err = writer.Close()
-  if err != nil {
-      return nil, err
+  writerClosingError := writer.Close()
+  if writerClosingError != nil {
+      return nil, writerClosingError
   }
 
   //CREATE REQUEST
-  req, err := http.NewRequest("POST", targetURL, requestBody)
+  
+  req, requestCreationError := http.NewRequest("POST", targetURL, requestBody)
   req.Header.Set("Content-Type", writer.FormDataContentType())
-  return req, err
+  return req, requestCreationError
+  
 }
 
 
 func main2() {
   //path, _ := os.Getwd()
   //path += "/test.pdf"
+originalFilePath := os.Args[1]
+repetitions,_ := strconv.Atoi(os.Args[2])
 
-  request, err := createFileUploadRequest("http://localhost:8080/assets",  "file", "C://test.txt",500)
+fmt.Println("START LOAD TEST WITH REQUEST WITH ",repetitions," REPETITIONS")
+  request, err := createFileUploadRequest("http://localhost:8080/assets/", originalFilePath,repetitions)
   if err != nil {
       log.Fatal(err)
   }
@@ -98,8 +121,9 @@ func main2() {
           log.Fatal(err)
       }
     resp.Body.Close()
-      fmt.Println(resp.StatusCode)
-      fmt.Println(resp.Header)
-      fmt.Println(body)
+
+    fmt.Println("STAUS CODE:",resp.StatusCode)
+    fmt.Println("RESPONSE HEADER: ",resp.Header)
+    fmt.Println("BODY: ",body)
   }
 }
